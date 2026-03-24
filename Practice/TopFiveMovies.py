8641ac3ef229906f3704ea86c4d7cb18921c1741
WilliamHudson26 <825426@mystma.org>
2026-03-24 15:12:15 -0500

    Top Five

---
+++ Practice/TopFiveMovies.py
@@ -0,0 +1,6 @@
+1 = "Jurrasic Park"
+2 = "Star Wars"
+3 = "Godzilla (2014)"
+4 = "War Of The Worlds (2005)"
+5 = "Lord Of The Rings"
+print ("Top Five Moveies Of All Time.","#5:",5,"#4:",4,"#3",3,"#2:",2,"#1:",1,"This has been Top Five Movies Of All Time")
