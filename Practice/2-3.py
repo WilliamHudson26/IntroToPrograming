bb70086b7d2c3594133ca044f4c9742541e23f37
WilliamHudson26 <825426@mystma.org>
2026-03-25 15:10:06 -0500

    favorite snack

---
+++ Practice/2-3.py
@@ -0,0 +1,17 @@
+#x = 1
+#while (x < 256):
+    #print(x)
+    #x += 1
+    #if (x == 1):
+        #break
+#print("I counted to the 8-bit intiger limit!")
+#am_i_within_melee_range_of_the_player = 1 #snake case (prefered)
+#amIWithinMeleeRangeOfThePlayer = 1 #came case
+#AMIWITHINMELEERANGEOFTHEPLAYER = 1 #uppercase
+#amiwithinmeleerangeoftheplayer = 1 #lowercase
+
+print("what is your favorite snack?")
+fav_snack = input()
+
+print("your fav snack is")
+print(fav_snack)
