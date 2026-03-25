c542331f766d5bab755865347c4c3bade50a2219
WilliamHudson26 <825426@mystma.org>
2026-03-25 14:12:54 -0500

    mic update1

--- Practice/TopFiveMovies.py
+++ Practice/TopFiveMovies.py
@@ -1,6 +1,6 @@
-1 = "Jurrasic Park"
-2 = "Star Wars"
-3 = "Godzilla (2014)"
-4 = "War Of The Worlds (2005)"
-5 = "Lord Of The Rings"
-print ("Top Five Moveies Of All Time.","#5:",5,"#4:",4,"#3",3,"#2:",2,"#1:",1,"This has been Top Five Movies Of All Time")
+one = "Jurrasic Park"
+two = "Star Wars"
+three = "Godzilla (2014)"
+four = "War Of The Worlds (2005)"
+five = "Lord Of The Rings"
+print ("Top Five Moveies Of All Time.","\n#5:",five,"\n#4:",four,"\n#3",three,"\n#2:",two,"\n#1:",one,"\nThis has been Top Five Movies Of All Time")
