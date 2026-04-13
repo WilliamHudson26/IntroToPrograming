9640847ab6cc3b2648669100839bbdb468e73e40
WilliamHudson26 <825426@mystma.org>
2026-04-13 14:17:04 -0500

    Huricane Classifier

---
+++ Assignments/Huricane_Classifier.py
@@ -0,0 +1,18 @@
+MPH = int(input("input the storms windspeed (MPH)\n->"))
+if MPH < 74:
+    category = "Tropical Storm"
+elif MPH < 96:
+    category = "Category 1"
+elif MPH < 111:
+    category = "Category 2"
+elif MPH < 130:
+    category = "Category 3"
+elif MPH < 157:
+    category = "Category 4"
+elif MPH >= 157:
+    category = "Category 5"
+
+if category == "Tropical Storm":
+    print("You have recorded a tropical storm.")
+else:
+    print("You have recorded a " + category + " Huricane!")
