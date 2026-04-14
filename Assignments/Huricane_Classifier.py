5fc0b1062f6997605347ef0abc090e3744352bc1
WilliamHudson26 <825426@mystma.org>
2026-04-14 15:04:51 -0500

    Exception Handling

--- Assignments/Huricane_Classifier.py
+++ Assignments/Huricane_Classifier.py
@@ -1,18 +1,27 @@
-MPH = int(input("input the storms windspeed (MPH)\n->"))
-if MPH < 74:
-    category = "Tropical Storm"
-elif MPH < 96:
-    category = "Category 1"
-elif MPH < 111:
-    category = "Category 2"
-elif MPH < 130:
-    category = "Category 3"
-elif MPH < 157:
-    category = "Category 4"
-elif MPH >= 157:
-    category = "Category 5"
+def main():
+    category = "Null"
+    try:
+        MPH = int(input("input the storms windspeed (MPH)\n->"))
+        if MPH <=0:
+            raise ValueError
+        if MPH < 74:
+            category = "Tropical Storm"
+        elif MPH < 96:
+            category = "Category 1"
+        elif MPH < 111:
+            category = "Category 2"
+        elif MPH < 130:
+            category = "Category 3"
+        elif MPH < 157:
+            category = "Category 4"
+        elif MPH >= 157:
+            category = "Category 5"
 
-if category == "Tropical Storm":
-    print("You have recorded a tropical storm.")
-else:
-    print("You have recorded a " + category + " Huricane!")
+        if category == "Tropical Storm":
+            print("You have recorded a tropical storm.")
+        else:
+            print("You have recorded a " + category + " Huricane!")
+    except ValueError:
+        print("Invalid")
+        main()
+main()
