0f065adb48a0689cb3fc313438b91b0d79d88b2e
WilliamHudson26 <825426@mystma.org>
2026-04-14 15:05:26 -0500

    mic update 2

---
+++ Practice/3-4.py
@@ -0,0 +1,18 @@
+def get_number():
+    n = input("Enter a whole number")
+    try:
+        n = int(n)
+    except:
+        print("invalid, try again...")
+        get_number()
+    return n
+
+def divide(x):
+    try:
+        return 100/x
+    except:
+        print("invalid, try again...")
+        get_number()
+
+num = get_number()
+print(divide(num))
