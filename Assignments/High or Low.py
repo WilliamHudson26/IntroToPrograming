3b8ccd46033bc6e4ef70b3da02006d64ec435a7d
WilliamHudson26 <825426@mystma.org>
2026-05-20 15:07:08 -0500

    High or Low

---
+++ Assignments/High or Low.py
@@ -0,0 +1,36 @@
+import random
+
+number = random.randint(1,100)
+answer = "null"
+score = 0
+name = input("Enter your username\n->")
+print("Guess The Number!")
+
+while answer != number:
+    try:
+        answer = int(input("->"))
+        if answer == number:
+            print("Thats It!")
+        elif answer > 100 or answer < 1:
+            print("Your not within range")
+        elif answer == (number+1):
+            print("your just above it!")
+        elif answer == (number-1):
+            print("your just bellow it!")
+        elif answer < (number+10) and answer > (number):
+            print("A bit lower!")
+        elif answer > (number-10) and answer < (number):
+            print("A bit higher!")
+        elif answer < (number+30) and answer > (number):
+            print("Lower, but you are getting close!")
+        elif answer > (number-30) and answer < (number):
+            print("Higher, but you are getting close!")
+        elif answer < (number+100) and answer > (number):
+            print("Lower...")
+        elif answer > (number-100) and answer < (number):
+            print("Higher...")
+    except ValueError:
+        print("that is not a number!")
+    score += 1
+    with open("C:\\Users\\825426\\Documents\\Intro to Programing\\IntroToPrograming\\Assignments\\HighorLowScores.txt", "r") as file:
+        file.write("\n" + name + str(score))
--- Assignments/Wheres Waldo.py
+++ Assignments/Wheres Waldo.py
@@ -1,10 +1,13 @@
 with open("C:\\Users\\825426\\Documents\\Intro to Programing\\IntroToPrograming\\Assignments\\names.txt", "r") as file:
     lines = file.readlines()
     waldo = False
-    for line in lines:
-        if line == 'Waldo\n':
+    line_count = 0
+    for i in lines:
+        if i.strip().lower() == 'Waldo\n':
+            line_count += 1
             waldo = True
+            break
     if waldo == True:
-        print("Found Waldo!")
+        print("Found Waldo!" + str(line_count) + "!")
     else:
         print("waldo not found...")
