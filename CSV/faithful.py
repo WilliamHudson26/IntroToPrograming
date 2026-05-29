17209dfeae3155eea09390e26bd4a79cd08a1e7d
WilliamHudson26 <825426@mystma.org>
2026-05-29 15:08:36 -0500

    Back on Track

--- CSV/faithful.py
+++ CSV/faithful.py
@@ -1,4 +1,4 @@
-import csv #6 points
+import csv #7 points
 with open("C:\\Users\\825426\\Documents\\Intro to Programing\\IntroToPrograming\\CSV\\faithful.csv", "r") as file:
     table = csv.DictReader(file)
     wait_time = []
@@ -13,17 +13,13 @@ with open("C:\\Users\\825426\\Documents\\Intro to Programing\\IntroToPrograming\
         wait_time.append(row['Wait'])
         total_wait += int(row['Wait'])
         rows += 1
-    for row in table:
-        print(row['Wait'])
-        if " " + row['Wait'] == min(wait_time):
-            long_wait_eruption.append(row['Length'])
-            print("yes")
-        continue
-    print(min(wait_time))
+        if row['Wait'] == min(wait_time):
+            long_wait_eruption = row['Length']
+
     print("The average length was " + str(round(total_length/rows)) + " minutes!")
-    print("The longest eruption time was " + str(max(eruption_time)) + " minutes!")
-    print("The shortest eruption time was " + str(min(eruption_time)) + " minutes!")
+    print("The longest eruption time was" + str(max(eruption_time)) + " minutes!")
+    print("The shortest eruption time was" + str(min(eruption_time)) + " minutes!")
     print("The average wait time was " + str(round(total_wait/rows)) + " minutes!")
-    print("The longest wait time was " + str(max(wait_time)) + " minutes!")
-    print("The shortest wait time was " +str(min(wait_time)) + " minutes!")
-    print("The eruption time of the eruption with the longest wait was " + str(long_wait_eruption) + " minutes!")
+    print("The longest wait time was" + str(max(wait_time)) + " minutes!")
+    print("The shortest wait time was" +str(min(wait_time)) + " minutes!")
+    print("The eruption time of the eruption with the longest wait was" + str(long_wait_eruption) + " minutes!")
--- CSV/occupation.py
+++ CSV/occupation.py
@@ -1,8 +1,33 @@
-import csv #1 points
+import csv #7 points ; 6 points left
 with open("C:\\Users\\825426\\Documents\\Intro to Programing\\IntroToPrograming\\CSV\\occupation-2018-census-csv.csv","r",encoding = "utf-8") as file:
     table = csv.DictReader(file)
+    grape_grower = "null"
+    oc_with_num = "null"
+    oc_with_code = "null"
+    common = []
+    occupation = []
+    oc_max = "null"
+    oc_min = "null"
 
     for row in table:
+        if row['Occupation'] == "Total" or row['Occupation'] == "Total stated" or row['Occupation'] == "Not stated" or row['Occupation'] == "Response outside scope" or row['Occupation'] == "Response unidentifiable":
+            continue
+        common.append(int(row['Employe']))
+        occupation.append(row['Occupation'])
         if row['Occupation'] == "Grape Grower":
-            grape_grower = str(row['Employed_census_usually_resident_population_count_aged_15_years_and_over'])
-        print("There are " + grape_grower + " grape growers")
+            grape_grower = str(row['Employe'])
+        if row['Employe'] == "14298":
+            oc_with_num = row['Occupation']
+        if row['Code'] == "451311":
+            oc_with_code = row['Occupation']
+        if int(row['Employe']) == max(common):
+            oc_max = row['Occupation']
+        if int(row['Employe']) == min(common):
+            oc_min = row['Occupation']
+                
+        
+    print("The most common occupation is "+ oc_max +" with "+ str(max(common)) +" employees")
+    print("The most common occupation is "+ oc_min +" with "+ str(min(common)) +" employees")
+    print("There are " + grape_grower + " grape growers")
+    print("The occupation with 14298 employees is " + oc_with_num)
+    print("The occupation with the code 451311 is " + oc_with_code)
